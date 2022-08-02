6d4d3a73c333b70826fd26ead1cd71bbeb267a59
Elisabeth Sáez <eesaez.munoz@gmail.com>
2022-08-02 19:16:08 -0400

    Storage+JSON

--- string.js
+++ string.js
@@ -1,7 +1,7 @@
 /* Prototipo de mini juego se puede interactuar con él mediante
- las teclas arriba, abajo, derecha e izquierda, se incorporaron margenes
- para que el personaje principal sólo pueda transitar por el camino, se simplificó
- diseño por mientras se deciden las mecánicas del juego, se añadió música y un tesoro para recolectar y una meta*/
+ las teclas arriba, abajo, derecha e izquierda, se incorporaron baldozas con
+ diseños, se agrega diseño de enemigos (aún no se incorporan las colisiones con los enemigos)
+ se agrega además el nombre del usuario y se alamacena*/
 
 
 
@@ -12,30 +12,38 @@ void function () {
     let rhythm;
     let widthF = 50;
     let heightF = 50;
-    let lawn = '#5EC275';
-    let water = '#77E9EA';
-    let way = '#E1CB85';
-    let finish = '#534007';
-    let myPrecious = '#0D2AFF';
+    let lawn;
+    let water;
+    let way;
+    let finish;
+    let myPrecious;
+    let tileMap;
     let scene = [
-        [0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 1, 1, 0, 1, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
-        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
-        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
-        [0, 2, 2, 0, 0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 2, 2, 0, 0, 2, 2, 1, 0, 0, 0, 1, 1, 1, 1],
-        [0, 2, 2, 0, 0, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 2, 3, 0, 0, 0, 0, 2, 2, 2, 0],
+        [0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 2, 2, 2, 1],
+        [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
 
     const FPS = 50;
 
+
+
     onload = function () {
-        canvas = document.getElementById('canvas');        
+        verStorage();
+        loadPlayerName();
+        canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
+
+        tileMap = new Image();
+        tileMap.src = 'img/tilemap.png';
         setInterval(function () {
             main();
         }, 1000 / FPS);
@@ -43,23 +51,49 @@ void function () {
         rhythm = new Howl({
             src: ['sound/fondo.mp3'],
             loop: true,
-            volume:0.5
+            volume: 0.5
         }
         );
         rhythm.play();
 
         let titulo = document.getElementById('title');
         console.log(titulo.innerText);
-        titulo.innerText='Prototipo de juego';
+        titulo.innerText = 'Prototipo de juego';
         console.log(titulo.innerText);
     }
 
     let p1 = new Enemy(60, 150);
     let p2 = new Enemy(50, 100);
     let p3 = new Enemy(70, 270);
-    let p4 = new Enemy(30, 10);
-    let p5 = new Enemy(500, 310);
+    let p4 = new Enemy(600, 10);
+    let p5 = new Enemy(450, 310);
     let hero = new player();
+
+    function verStorage() {
+        let st = localStorage.getItem("jugador");
+        
+        if (st == null) {
+            let saludo = prompt("¿Cuál es tu nombre?");
+            arr = {
+                nombre: saludo,
+                vida: 100
+            };
+            localStorage.setItem("jugador", JSON.stringify(arr));
+        }
+    }
+
+    function loadPlayerName(){
+        let st = localStorage.getItem("jugador");
+        let obj = JSON.parse(st);
+        let promVal = document.getElementById("hi");
+        
+        if(obj.nombre == ''){
+            promVal.textContent = "invitado"
+        } else {
+            promVal.textContent = obj.nombre;
+        }      
+    
+    }
 
     function main() {
         deleteCanvas();
@@ -78,26 +112,12 @@ void function () {
     }
 
     function paintScene() {
-        let color;
+
         for (y = 0; y < 11; y++) {
             for (x = 0; x < 15; x++) {
-                if (scene[y][x] == 0) {
-                    color = lawn;
-                }
-                if (scene[y][x] == 1) {
-                    color = water;
-                }
-                if (scene[y][x] == 2) {
-                    color = way;
-                }
-                if (scene[y][x] == 3) {
-                    color = finish;
-                }
-                if (scene[y][x] == 4) {
-                    color = myPrecious;
-                }
-                ctx.fillStyle = color;
-                ctx.fillRect(x * widthF, y * heightF, widthF, heightF);
+                let tile = scene[y][x];
+
+                ctx.drawImage(tileMap, tile * 32, 0, 32, 32, widthF * x, heightF * y, widthF, heightF);
             }
         }
     }
@@ -121,13 +141,12 @@ void function () {
     }
 
     function player() {
-        this.x = 1;
+        this.x = 2;
         this.y = 9;
         this.color = '#A160EB';
 
         this.margin = function (x, y) {
             let collision = false;
-            if (scene[y][x] == 1) { collision = true; }
             if (scene[y][x] == 0) { collision = true; }
             return (collision);
         }
@@ -158,28 +177,25 @@ void function () {
 
         this.logic = function () {
             let ver = scene[this.y][this.x];
-            if (ver == 4) {
+            if (ver == 3) {
                 this.myPrecious = true;
                 scene[this.y][this.x] = 2;
-                alert('¡Has conseguido el tesoro!');
+                alert('¡Has conseguido la llave!');
             }
-            if (ver == 3 && this.myPrecious !== true) {
-                alert('Necesitas el tesoro para ganar');
-                
-            } 
-            if (ver == 3 && this.myPrecious == true) {
-                alert('¡Has ganado!');
-                
+            if (ver == 1 && this.myPrecious !== true) {
+                alert('Necesitas la llave para entrar');
+
+            }
+            if (ver == 1 && this.myPrecious == true) {
+                alert('¡Has entrado!');
+
             }
         }
 
         this.draw = function () {
             let img = new Image();
             img.src = 'img/gatito.png';
-            ctx.drawImage(img, this.x * widthF - 50, this.y * heightF - 45);
-
-            //ctx.fillStyle = this.color;
-            //ctx.fillRect(this.x * widthF, this.y * heightF, widthF, heightF);
+            ctx.drawImage(img, this.x * widthF - 20, this.y * heightF - 50);
         }
 
     }
@@ -190,10 +206,22 @@ void function () {
         this.shift = true;
 
         this.draw = function () {
-            ctx.fillStyle = '#858175';
-            ctx.fillRect(this.x, this.y, 30, 30);
+
+            let img2 = new Image();
+            img2.src = 'img/ghost.png';
+            ctx.drawImage(img2, this.x, this.y);
+
         }
 
+        /* this.verCol = function (x, y) {
+            let paw = false;
+
+            if( scene[y][x]==0){
+                paw =true
+            }
+            return paw;
+        }
+ */
         this.moveSide = function (speed) {
             if (this.shift == true) {
                 if (this.x < 540) {
@@ -225,6 +253,8 @@ void function () {
                 }
             }
         }
+
+
     }
 
     function deleteCanvas() {
